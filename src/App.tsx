e50268b396d66198a79392d75dc7e27cbf45928e
elvis.pinheiro <elvis.pinheiro1996@gmail.com>
2022-11-30 22:32:08 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Rotas } from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function App() {
+
+  // github.com/profchines
+  return (
+    <>
+      <Rotas />
+    </>
+  );
+}
+
+export default App;
